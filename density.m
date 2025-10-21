6019f87bc49bb47ed537879030efedd5a0ce7161
Cayden Varno <cayden.varno@gmail.com>
2025-10-20 20:22:40 -0400

    Density done baybee

--- density.m
+++ density.m
@@ -1,7 +1,33 @@
-%{
+% *************************************************************************
+% Purdue Orbital, Flight Dynamics, Ascent Modeling
+% 
+% Function Name: density
+% File Name: density.m
+%
+% Contributors: Cayden
+% Date Started: 10/20/2025
+% Last Updated: 10/20/2025
+%
+% Function Description: This function accepts an input of pressure,
+% molecular weight, and temperature. Gas constant is hardcoded internally.
+% Density is returned in kg/m^3.
+%
+% Output variables: 
+% - density
+% 
+% *************************************************************************
 
-TODO add detailed description of what the file does here
+% *************************************************************************
+% 
+% Constants
+% - Gas Constant = 8.31432e3 N * m / (kmol * k)
+% 
+% *************************************************************************
 
-Contributors:
+function density = density(pressure, molWeight, temp)
 
-%}
+gas_const = 8.31432e3;
+
+density = pressure * molWeight / gas_const / temp;
+
+end
