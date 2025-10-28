166c33b2bc3caa7fe8968b1ef4baa9300caf6a6a
Aanand Shah <shahaanand711@gmail.com>
2025-10-27 20:11:06 -0400

    forgor to save bouyantForce

--- modules/buoyantForce.m
+++ modules/buoyantForce.m
@@ -9,19 +9,18 @@ Function: This calculates the bouyant force acting on the balloon. Pulls in
 Contributors: Aanand Shah
 
 %}
-function F_buoyant = buoyantForce(volume, densityAir, gravity, external_pressure, temperature,)
+function F_buoyant = buoyantForce(volume, densityAir, gravity, external_pressure, temperature)
     % volume in m^3
     % fluidDensity in kg/m^3
     % gravity in m/s^2
 
-    universal_gas_constant = 8.314462618; % J/(mol·K)
-    molar_mass_gas = 0.004002602; % kg/mol for Helium
-    
-    densityBalloon = densityBalloon(external_pressure,temperature); % in kg/m^3 
 
-    densityAir = densityAir(pressure, molWeight, temp); % in kg/m^3
+    densityB = densityBalloon(external_pressure,temperature); % in kg/m^3 
 
+    densityA = densityAir(pressure, molWeight, temp); % in kg/m^3
+
+    volume =   volume(densityB); % in m^3 
     % Calculate the buoyant force using Archimedes' principle
-    F_buoyant = (densityAir-densityBalloon) * volume * gravity; % in Newtons (N)
+    F_buoyant = (densityA-densityB) * volume * gravity; % in Newtons (N)
 
 end
