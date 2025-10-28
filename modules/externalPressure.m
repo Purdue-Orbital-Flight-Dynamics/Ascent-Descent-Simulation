bd77e272340737034d00f0791dbcc7bff06b8816
Cayden Varno <cayden.varno@gmail.com>
2025-10-27 20:04:59 -0400

    geopot ext pres

--- modules/externalPressure.m
+++ modules/externalPressure.m
@@ -1,8 +1,13 @@
+function pressure = externalPressure(altitude, T_calculated, T_initial, Lmb)
+% alt in geopot
+
+
 %pressurefunctionforOrbital 
 %Authors: Garion, Liam
 
 
 R = 287;   
+g0 = 9.80665;
 
 %lmb is 'a', or the slope
 
@@ -11,19 +16,19 @@ R = 287;
 if altitude < 11100 %gradient 1
     
     pressure_initial = 1.01325*10^3; % mBar
-    pgradient = pressure_initial*(T_calculated/T_initial)^-(g0/Lmb/R);
+    pressure = pressure_initial*(T_calculated/T_initial)^-(g0/Lmb/R);
 
-elseif altitude < 20000 % Pause one
+elseif altitude < Z2H(20000) % Pause one
     pressure_initial = 2.2346*10^2;
-    ppause = pressure_initial * e.^-((g0/R/216.65)*(altitude*1000-11100)); % 216 is the pause temp
+    pressure = pressure_initial * e.^-((g0/R/216.65)*(altitude*1000-11100)); % 216 is the pause temp
 
-elseif altitude < 47400 %Gradient 2
+elseif altitude < Z2H(47400) %Gradient 2
     pressure_initial = 5.5292*10;
-    pgradient = pressure_initial*(T_calculated/T_initial)^-(g0/Lmb/R);
+    pressure = pressure_initial*(T_calculated/T_initial)^-(g0/Lmb/R);
 
-elseif altitude < 51000 % Pause 2
+elseif altitude < Z2H(51000) % Pause 2
     pressure_initial = 7.0458 * 10^-1;
-    ppause = pressure_initial * e.^-((g0/R/270.65)*(altitude*1000-11100)); % 270.65is the pause temp
+    pressure = pressure_initial * e.^-((g0/R/270.65)*(altitude*1000-11100)); % 270.65is the pause temp
     
 end
 
