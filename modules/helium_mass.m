3482d492bac126f0c7d028976c00f16eedf187f5
liambshepard <liambshepard@gmail.com>
2025-11-10 20:34:38 -0500

    tr

--- modules/helium_mass.m
+++ modules/helium_mass.m
@@ -10,7 +10,7 @@ Samuel Landers
 %}
 
 % (1) calculate temperature and pressure
-[temperature_K, t_initial, slope_variable]= temperature(initial_altitude);
+[temperature_K, t_initial, slope_variable] = temperature(initial_altitude);
 temp_data = [temperature_K, t_initial, slope_variable];
 temp = temp_data(1);
 temp_inital = temp_data(2);
