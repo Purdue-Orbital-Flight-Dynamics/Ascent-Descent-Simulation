99a127bb23ef14e2af12b6524bd88cdd27e7b684
SamuelRLanders <156484367+SamuelRLanders@users.noreply.github.com>
2025-11-12 20:33:24 -0500

    cross sectional area

--- modules/cross_sectional_area_parachute.m
+++ modules/cross_sectional_area_parachute.m
@@ -10,22 +10,5 @@ Garion Cheng
 Samuel Landers
 %}
 
-% (1) calculate temperature and pressure
-temp_data = temperature(altitude);
-t = temp_data(1);
-temp_inital = temp_data(2);
-slope = temp_data(3);
-p = external_pressure(altitude, T, temp_inital, slope);
-
-% (2) calculate mols of helium
-HELIUM_MOLAR_MASS = 0.00400261;
-n = helium_mass / HELIUM_MOLAR_MASS;
-
-% (3) utilize ideal gas law to calculate volume of gas
-R = 8.314; %J/(mol*k)
-v = n*R*t/p;
-
-% (4) use volume to calculate cross setional area of the sphere
-radius = (3 * v / (4 * pi))^(1 / 3);
-area = pi * (radius^2);
+area = 10;
 end
