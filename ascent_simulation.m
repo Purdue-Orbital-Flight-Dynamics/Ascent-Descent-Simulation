bf700c2f400ef1bf41cb6ab52734a7fce8ecd1f7
eqwert12 <ummingereric@gmail.com>
2025-11-12 19:33:35 -0500

    sadggsdgas

--- ascent_simulation.m
+++ ascent_simulation.m
@@ -53,7 +53,7 @@ for initial_buoyant_force = buoyant_force_start_index:0.0001:buoyant_force_end_i
         end
 
         % --- Forces
-        drag_force          = dragForce(velocity);
+        drag_force          = dragForce(velocity, helium_mass, position);
         gravitational_force = gravitationalForce(position, total_mass);
         buoyant_force       = buoyantForce(altitude, helium_mass);
         
