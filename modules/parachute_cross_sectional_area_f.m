1750918ad7a32ab3dbfea710a51aa930cbf52c85
Cayden Varno <cayden.varno@gmail.com>
2025-11-17 20:41:46 -0500

    parachute

--- modules/parachute_cross_sectional_area_f.m
+++ modules/parachute_cross_sectional_area_f.m
@@ -1,16 +1,32 @@
-function area = cross_sectional_area_parachute()
-%{
-Calculates the cross sectional area of the parachute.
+function [area] = parachute_cross_sectional_area_f()
 
-Samuel Landers
-%}
+%************************************************************************
+% Purdue Orbital, Flight Dynamics
+%
+% Project Name: Ascent/Descent Modeling
+%
+% Function Name: parachute_cross_sectional_area_f
+% File Name: parachute_cross_sectional_area_f.m
+%
+% Contributors: Samuel Landers
+% Date Created: 11/??/2025
+% Last Updated: 11/17/2025
+%
+% Function Description:
+%   Returns the cross-sectional area of the parachute. The parachute size
+%   is currently hardcoded based on expected mission design options.
+%
+% References:
+%
+% Input variables:
+%   None
+%
+% Output variables:
+% - area: parachute cross-sectional area, m^2, positive
+%
+%************************************************************************
 
-% there are multiple parachute sizes we will use
-% those are hardcoded here
+PARACHUTE_AREA_FT2 = 15; % ft^2, parachute planform area
+FT2_TO_M2 = 1 / 10.764; % m^2/ft^2
 
-area = 15; % ft^2
-
-% convert to metric units
-area = area / 10.764;
-
-end
+area = PARACHUTE_AREA_FT2 * FT2_TO_M2; % m^2
