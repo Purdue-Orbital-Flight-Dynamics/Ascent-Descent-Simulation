0f2ff3b6834452f44098597270f6632d2190889d
Cayden Varno <cayden.varno@gmail.com>
2025-11-19 20:14:17 -0500

    fix steps

--- ascent_simulation.m
+++ ascent_simulation.m
@@ -16,19 +16,18 @@ startup()
 
 best_buoyancy_force = NaN;
 closest_ascent_rate = Inf;
-buoyant_force_start_index = 200; % [N]
+buoyant_force_start_index = 0; % [N]
 buoyant_force_end_index = 600; % [N]
-dt         = 1;      % [s]
+dt         = .01;      % [s]
 stop_steps = 1000; % [s]
-buoyant_step = 50;
+buoyant_step = 1;
 
 % --- Inputs
 burst_altitude     = input("Enter balloon burst altitude (m)    : ");
 start_altitude     = input("Enter starting altitude (m)         : ");
 target_ascent_rate = input("Enter desired ascent rate (m/s)     : ");
-initial_buoyant_force = buoyant_force_start_index;
 
-%for initial_buoyant_force = buoyant_force_start_index:buoyant_step:buoyant_force_end_index
+for initial_buoyant_force = buoyant_force_start_index:buoyant_step:buoyant_force_end_index
 
     % --- Initial conditions
     cur_time      = 0;                    % [s]
@@ -51,30 +50,32 @@ initial_buoyant_force = buoyant_force_start_index;
     % --- Loop until balloon reaches burst altitude
     while position < burst_altitude
 
-        % --- Exit if taking too long
-        if (k >= stop_steps)
-            break;
-        end
+        % % --- Exit if taking too long
+        % if (k >= stop_steps)
+        %     break;
+        % end
 
         % --- Forces
         drag_force          = drag_force_f(velocity, helium_mass, position);
-        gravitational_force = -gravity_force_f(position, total_mass);
+        gravitational_force = gravity_force_f(position, total_mass);
         buoyant_force       = buoyant_force_f(position, helium_mass);
         
-        net_force           = buoyant_force - drag_force + gravitational_force;
+        
+
+        net_force           = buoyant_force - drag_force - gravitational_force;
         if net_force < 0
             break
         end
 
-        % --- Sanity checks
-        if net_force < 0
-            % Balloon should always be going up
-            break
-        elseif gravitational_force > 0
-            % Gravity should always be negative
-            disp("Error: gravity was positive");
-            return;
-        end
+        % % --- Sanity checks
+        % if net_force < 0
+        %     % Balloon should always be going up
+        %     break
+        % elseif gravitational_force > 0
+        %     % Gravity should always be negative
+        %     disp("Error: gravity was positive");
+        %     return;
+        % end
        
         % --- Update state
         acceleration = net_force / total_mass;
@@ -101,6 +102,7 @@ initial_buoyant_force = buoyant_force_start_index;
         closest_ascent_rate = avg_ascent_rate;
         best_buoyancy_force = initial_buoyant_force;
     end
-%end
+end
 
 disp(best_buoyancy_force + " N buoyancy force results in an avg. velocity of " + closest_ascent_rate + " m/s");
+disp(size(x))
