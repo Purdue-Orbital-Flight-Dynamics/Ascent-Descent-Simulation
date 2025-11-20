eff52a9dff0d3fd0ba6866f9330cda606b97d07a
eqwert12 <ummingereric@gmail.com>
2025-11-19 19:49:52 -0500

    adsfads

--- modules/helium_mass_f.m
+++ modules/helium_mass_f.m
@@ -1,4 +1,4 @@
-function [helium_mass] = helium_mass_f(altitude, buoyancy_force)
+function [helium_mass] = helium_mass_f(altitude, net_buoyancy_force)
 
 %************************************************************************
 % Purdue Orbital, Flight Dynamics
@@ -19,7 +19,7 @@ function [helium_mass] = helium_mass_f(altitude, buoyancy_force)
 %
 % Input variables:
 % - altitude: altitude, in meters, positive
-% - buoyancy_force: buoyancy force, in Newtons, positive
+% - net_buoyancy_force: force measured from force meter, in Newtons, positive
 %
 % Output variables: 
 % - helium_mass: helium mass, in kilograms, positive
@@ -35,9 +35,6 @@ air_density = air_density_f(altitude); % in kg/m^3
 % Acceleration
 gravity_acceleration = gravity_acceleration_f(altitude); % in m/s^2
 
-% Volume
-volume = buoyancy_force / (air_density - balloon_density) / gravity_acceleration; % in kg/m^3
-
 % Helium mass
-helium_mass = balloon_density * volume; % in kg
+helium_mass = (balloon_density * net_buoyancy_force) / (gravity_acceleration * (air_density - balloon_density)); % in kg
 
