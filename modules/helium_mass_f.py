19e1006946e7dbabd3b5c7579e5303dbbb94282c
Garion-Cheng <garioncheng@gmail.com>
2026-02-04 20:25:08 -0500

    Descent sim, and other 2 subfunctions added to python

---
+++ modules/helium_mass_f.py
@@ -0,0 +1,54 @@
+from modules.balloon_density_f import balloon_density_f
+from modules.air_density_f import air_density_f
+from modules.gravity_acceleration_f import gravity_acceleration_f
+
+def helium_mass_f(altitude, net_buoyancy_force):
+
+#************************************************************************
+# Purdue Orbital, Flight Dynamics
+# 
+# Project Name: Ascent Modeling
+# 
+# Function Name: helium_mass_f
+# File Name: helium_mass_f.m
+#
+# Contributors: Garion Cheng, Eric Umminger, Jack Triglianos, Samuel Landers
+# Date Created: 10/??/2025
+# Last Updated: 11/17/2025
+#
+# Function Description: This function will calculate the mass of helium given 
+# an altitude and buoyancy force.
+# 
+# References: N/A
+#
+# Input variables:
+# - altitude: altitude, in meters, positive
+# - net_buoyancy_force: force measured from force meter, in Newtons, positive
+#
+# Output variables: 
+# - helium_mass: helium mass, in kilograms, positive
+# 
+#************************************************************************
+
+    # Calculations
+
+    # Other mass
+    BALLOON_MASS = 0 # in kg
+    NECK_MASS = 0 # in kg, neck closure system only
+    ROPE_MASS = 0 # in kg, rope slack when inflating balloon
+    OTHER_MASS = BALLOON_MASS + NECK_MASS + ROPE_MASS
+
+    # Densities
+    balloon_density = balloon_density_f(altitude) # in kg/m^3
+    air_density = air_density_f(altitude) # in kg/m^3
+
+    # Acceleration
+    gravity_acceleration = gravity_acceleration_f(altitude) # in m/s^2
+
+    # Other weight
+    other_weight = OTHER_MASS * gravity_acceleration
+
+    # Helium mass
+    helium_mass = (balloon_density * (net_buoyancy_force + other_weight)) / (gravity_acceleration * (air_density - balloon_density)) # in kg
+
+    return helium_mass
---
+++ modules/system_m_f.py
@@ -0,0 +1,49 @@
+from modules.helium_mass_f import helium_mass_f
+
+def system_mass_f(initial_altitude, initial_buoyancy_force):
+
+#************************************************************************
+# Purdue Orbital, Flight Dynamics
+# 
+# Project Name: Ascent Modeling
+# 
+# Function Name: system_mass_f
+# File Name: system_mass_f.py
+#
+# Contributors: Sam Landers, Garion Cheng
+# Date Created: 10/??/2025
+# Last Updated: 2/4/2026
+#
+# Function Description: This function will calculate the total system 
+# mass of the balloon-rocket system.
+# 
+# References: N/A
+#
+# Input variables:
+# - initial_altitude: initial launch altitude, in meters, positive
+# - initial_buoyancy_force: initial buoyancy force, in Newtons, positive
+#
+# Output variables: 
+# - system_mass: total system mass, in kilograms, positive
+# - helium_mass: calculated helium mass, in kilograms, positive
+# 
+#************************************************************************
+
+    # Constants
+    LAUNCH_STRUCTURE_MASS = 2.2; # in kg
+    FLIGHT_OPERATIONS_MASS = 2.2; # in kg, does not include helium mass
+    AVIONICS_MASS = 2.2; # in kg
+    MISCELLANEOUS_MASS = 2.2; # in kg
+
+    # --- Calculations
+    helium_mass = helium_mass_f(initial_altitude, initial_buoyancy_force)  # kg
+
+    system_mass = (
+        LAUNCH_STRUCTURE_MASS
+        + FLIGHT_OPERATIONS_MASS
+        + AVIONICS_MASS
+        + MISCELLANEOUS_MASS
+        + helium_mass
+    )  # kg
+
+    return system_mass, helium_mass
